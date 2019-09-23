6e2f9f3e232a2a2950e2a83f301912eca225351e
Kirill <kirrilf@mail.ru>
2019-09-23 20:21:55 +0600

    first commit

---
+++ Deque/Deque/Deque.cpp
@@ -0,0 +1,189 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+template<typename T>
+class List {
+
+public:
+
+	List();
+	~List();
+	void pop_back();
+	void push_back(T data);
+	void pop_front();
+	void push_front(T data);
+	void clear();
+	int GetSize() { return Size; }
+	T& operator[](const int index);
+	
+
+private:
+
+	template<typename T>
+	class Node 
+	{
+	public:
+		Node* pNext;
+		Node* pPrev;
+		T data;
+
+		Node(T data = T(), Node* pNext = nullptr, Node* pPrev = nullptr)
+		{
+			this->data = data;
+			this->pNext = pNext;
+			this->pPrev = pPrev;
+		}
+
+		
+	};
+	int Size;
+	Node<T>* head;
+	Node<T>* tail;
+};
+
+template<typename T>
+List<T>::List() {
+	Size = 0;
+	head = nullptr;
+	tail = nullptr;
+}
+
+template<typename T>
+List<T>::~List() {
+	clear();
+}
+
+template<typename T>
+void List<T>::pop_back()
+{
+	if (!tail)
+		cout << "Empty" << endl;
+	else
+	{
+		Node<T>* temp = tail;
+		tail = tail->pPrev;
+		delete temp;
+		if (tail)
+			tail->pNext = nullptr;
+		Size--;
+	}
+}
+
+template<typename T>
+void List<T>::push_back(T data) {
+	Node<T>* temp = new Node<T>(data);
+	temp->pNext = nullptr;
+	temp->pPrev = nullptr;
+
+	if (tail == nullptr)
+	{
+		head = temp;
+		tail = temp;
+		head->pNext = tail;
+		tail->pPrev = head;
+	}
+	else
+	{
+		temp->pPrev = tail;
+		tail->pNext = temp;
+		tail = temp;
+	}
+	Size++;
+
+}
+template<typename T>
+void List<T>::pop_front()
+{
+	if (!head)
+		cout << "Empty" << endl;
+	else
+	{
+		Node<T>* temp = head;
+		head = head->pNext;
+		delete temp;
+		if (head)
+			head->pPrev = nullptr;
+		Size--;
+	}
+
+}
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+	Node<T>* temp = new Node<T>(data);
+	temp->pNext = nullptr;
+	temp->pPrev = nullptr;
+
+	if (head == nullptr)
+	{
+		head = temp;
+		tail = temp;
+		head->pNext = tail;
+		tail->pPrev = head;
+	}
+	else
+	{
+		temp->pNext = head;
+		head->pPrev = temp;
+		head = temp;
+	}
+	Size++;
+}
+
+template<typename T>
+void List<T>::clear()
+{
+	while (Size) 
+	{
+		pop_front();
+	}
+
+}
+
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+	
+	int counter = 0;
+	Node<T>* current = this->head;
+	while (current != nullptr) 
+	{
+		if (counter == index) 
+		{
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+
+
+}
+
+
+
+
+
+int main()
+{
+	List<int> lst;
+	lst.push_back(5);
+	lst.push_back(10);
+	lst.push_front(11);
+	lst.pop_front();
+	lst.pop_back();
+	lst.clear();
+	
+
+	for (int i = 0; i < lst.GetSize(); i++) {
+		cout << lst[i] << endl;
+	}
+	
+
+	return 0;
+}
+
---
+++ Stack And Back Polish/Stack And Back Polish/Stack And Back Polish.cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#include <iomanip>
+
+template <typename T>
+class Stack
+{
+private:
+	T* stackPtr; 
+	int size; 
+	int top; 
+public:
+	Stack(int = 40);
+	~Stack();
+	bool push(const T); 
+	bool pop();
+	int Top();
+	bool empty();
+	
+};
+
+template <typename T>
+Stack<T>::Stack(int s)
+{
+	size = s;   
+	stackPtr = new T[size]; 
+	top = -1; 
+}
+
+template <typename T>
+Stack<T>::~Stack()
+{
+	delete[] stackPtr; 
+}
+
+template <typename T>
+bool Stack<T>::push(const T value)
+{
+	if (top == size - 1)
+		return false;
+
+	top++;
+	stackPtr[top] = value;
+
+	return true;
+}
+
+template <typename T>
+bool Stack<T>::pop()
+{
+	if (top == -1)
+		return false; 
+
+	stackPtr[top] = 0;
+	top--;
+
+	return true; 
+}
+
+template<typename T>
+int Stack<T>::Top()
+{
+	return stackPtr[top];
+}
+
+template<typename T>
+bool Stack<T>::empty()
+{
+	if (top == -1) {
+		return false;
+	}
+	return true;
+
+}
+
+
+
+
+
+Stack <char> stckBackPolish(50);
+
+void conversion(char now) {
+
+	bool flag = false;
+	
+	while (!flag) {
+		if (!stckBackPolish.empty()) {
+			flag = stckBackPolish.push(now);
+			
+		}
+		else {
+			if ((now == '*' || now == '/') && (stckBackPolish.Top() == '+' || stckBackPolish.Top() == '-' || stckBackPolish.Top() == '(')) {
+				flag = stckBackPolish.push(now);
+
+			}
+			else if ((now == '+' || now == '-') && stckBackPolish.Top() == '(') {
+				flag = stckBackPolish.push(now);
+				
+			}
+			else if (now == '(') {
+				flag = stckBackPolish.push(now);
+				
+			}
+			else if ((now == '+' || now == '-') && (stckBackPolish.Top() == '+' || stckBackPolish.Top() == '-' || stckBackPolish.Top() == '*' || stckBackPolish.Top() == '/' || stckBackPolish.Top() == '^')) {
+				cout << (char) stckBackPolish.Top();
+				stckBackPolish.pop();
+				
+			}
+			else if (now == '^' && (stckBackPolish.Top() == '+' || stckBackPolish.Top() == '-' || stckBackPolish.Top() == '*' || stckBackPolish.Top() == '/' || stckBackPolish.Top() == '^')) {
+				flag = stckBackPolish.push(now);
+
+			}
+			else if ((now == '*' || now == '/') && (stckBackPolish.Top() == '*' || stckBackPolish.Top() == '/' || stckBackPolish.Top() == '^')) {
+				cout << (char) stckBackPolish.Top();
+				stckBackPolish.pop();
+				
+			}
+			else if (now == ')' && stckBackPolish.Top() == '(') {
+				flag = stckBackPolish.pop();
+				
+			}
+			else if (now == ')') {
+				cout << (char) stckBackPolish.Top();
+				stckBackPolish.pop();
+			}
+
+
+		}
+
+
+	}
+
+
+
+}
+
+
+void input() {
+	string str;
+	cin >> str;
+	bool flag = false;
+	int number = 0;
+	for (auto now : str) {
+		if (now - '0' >= 0 && now - '0' <= 9) {
+			number *= 10;
+			number += (now - '0');
+			flag = true;
+		}
+		else if (now == '+' || now == '-' || now == '*' || now == '/' || now == '(' || now == ')' || now == '^') {
+			if (flag) {
+				cout << number;
+				flag = false;
+			}
+			number = 0;
+			conversion(now);
+		}
+
+	}
+
+	if (flag) {
+		cout << number;
+	}
+	while (stckBackPolish.empty()) 
+	{
+		cout << (char) stckBackPolish.Top();
+		stckBackPolish.pop();
+	}
+
+
+
+
+}
+
+
+
+
+int main()
+{
+	
+
+	input();
+	
+
+	return 0;
+}
+
